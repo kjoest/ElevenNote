28ab331e0d043aeaa831f91a7435c847b278f1ac
kyle Joest <kjoest92@gmail.com>
2021-08-18 16:41:29 -0500

    Commit Changes

--- ElevenNote.WebAPI/App_Start/SwaggerConfig.cs
+++ ElevenNote.WebAPI/App_Start/SwaggerConfig.cs
@@ -2,6 +2,12 @@ using System.Web.Http;
 using WebActivatorEx;
 using ElevenNote.WebAPI;
 using Swashbuckle.Application;
+using System.Linq;
+using Swashbuckle.Swagger;
+using System.Collections.Generic;
+using System.Web.Http.Description;
+using System.Web.Http.Filters;
+
 
 [assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 
@@ -9,6 +15,74 @@ namespace ElevenNote.WebAPI
 {
     public class SwaggerConfig
     {
+        /// <summary>
+        /// Document filter for adding Authriztion header in Swashbuckle / Swagger.
+        /// </summary>
+        
+        public class AddAuthorizationHeaderParameterOperationFilter : IOperationFilter
+        {
+            public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+            {
+                var filterPipelin = apiDescription.ActionDescriptor.GetFilterPipeline();
+                var isAuthorized = filterPipelin
+                    .Select(filterInfo => filterInfo.Instance)
+                    .Any(filter => filter is IAuthorizationFilter);
+
+                var allowAnonymous = apiDescription.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Any();
+
+                if (!isAuthorized || allowAnonymous) return;
+
+                if (operation.parameters == null) operation.parameters = new List<Parameter>();
+
+                operation.parameters.Add(new Parameter
+                {
+                    name = "Authorization",
+                    @in = "header",
+                    description = "from /token endpoint",
+                    required = true,
+                    type = "string"
+                });
+            }
+
+            ///<summary>
+            ///Document filter for adding OAuth Token endpoint documentation in Swashbuckle / Swagger.
+            ///Swagger normally won't find it - the /token endpoint - due to it being programmatically generated.
+            ///</summary>
+            
+            class AuthTokenEndpointOperation : IDocumentFilter
+            {
+                public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+                {
+                    swaggerDoc.paths.Add("/token", new PathItem
+                    {
+                        post = new Operation
+                        {
+                            tags = new List<string> { "Auth" },
+                            consumes = new List<string>
+                            {
+                                "application/x-www-form-urlencoded"
+                            },
+                            parameters = new List<Parameter> {
+                                new Parameter
+                                {
+                                    type = "string",
+                                    name = "grant_type",
+                                    required = true,
+                                    @in = "formData"
+                                },
+                                new Parameter
+                                {
+                                    type = "string",
+                                    name = "password",
+                                    required = false,
+                                    @in = "formData"
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        }
         public static void Register()
         {
             var thisAssembly = typeof(SwaggerConfig).Assembly;
