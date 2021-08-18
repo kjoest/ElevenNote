84e5840b7ac2d2856ccb923e642f5fa5d898df17
kyle Joest <kjoest92@gmail.com>
2021-08-17 19:56:55 -0500

    Commit Changes

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -50,11 +50,21 @@ namespace ElevenNote.WebAPI.Controllers
         public IHttpActionResult Put(NoteEdit note)
         {
             if (!ModelState.IsValid)
-                return BadRequest();
+                return BadRequest(ModelState);
 
             var service = CreateNoteService();
 
             if (!service.UpdateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateNoteService();
+
+            if (!service.DeleteNote(id))
                 return InternalServerError();
 
             return Ok();
